03d89757777abc82c7395ad3a6e904206e79b62c
Paul Hampson <528002+paulhampson@users.noreply.github.com>
2025-04-08 13:24:38 +0100

    feat: add vertical scrolling to the about page.

--- src/hmi/screens/settings_screens/about.rs
+++ src/hmi/screens/settings_screens/about.rs
@@ -16,22 +16,76 @@ use crate::application::application_state::ApplicationState;
 use crate::hmi::messaging::{UiActionChannelPublisher, UiActionsMessage};
 use crate::hmi::screens::{
     draw_message_screen, draw_message_screen_no_reformat, UiDrawer, UiInput, UiInputHandler,
+    DEFAULT_TEXT_STYLE,
 };
+use core::cmp::min;
 use core::fmt::Write;
 use embedded_graphics::draw_target::DrawTarget;
+use embedded_graphics::geometry::AnchorY;
 use embedded_graphics::pixelcolor::BinaryColor;
+use embedded_graphics::prelude::{DrawTargetExt, Point};
+use embedded_graphics::text::renderer::TextRenderer;
+use embedded_layout::View;
 use heapless::String;
 
 pub struct AboutScreen {
-    page_index: usize,
-    max_pages: usize,
+    start_entry_index: usize,
+    max_entries: usize,
+
+    repository_url_scroll_pos: usize,
 }
 
 impl AboutScreen {
     pub fn new() -> Self {
         Self {
-            page_index: 0,
-            max_pages: 1,
+            start_entry_index: 0,
+            max_entries: 4,
+
+            repository_url_scroll_pos: 0,
+        }
+    }
+
+    fn line_draw<D>(&self, display: &mut D, index: usize) -> Result<(), D::Error>
+    where
+        D: DrawTarget<Color = BinaryColor>,
+    {
+        let mut message_string = String::<100>::new();
+        match index {
+            0 => {
+                write!(
+                    message_string,
+                    "{} v{}",
+                    built_info::PKG_NAME,
+                    built_info::PKG_VERSION,
+                )
+                .expect("String too long");
+                draw_message_screen_no_reformat(display, &message_string)
+            }
+            1 => {
+                let dirty_indicator = if built_info::GIT_DIRTY.unwrap_or(true) {
+                    "+"
+                } else {
+                    ""
+                };
+                write!(
+                    message_string,
+                    "{}{}",
+                    built_info::GIT_COMMIT_HASH_SHORT.unwrap_or("unknown"),
+                    dirty_indicator
+                )
+                .expect("String too long");
+                draw_message_screen_no_reformat(display, &message_string)
+            }
+            2 => {
+                write!(message_string, "License: {}", built_info::PKG_LICENSE,)
+                    .expect("String too long");
+                draw_message_screen_no_reformat(display, &message_string)
+            }
+            3 => {
+                write!(message_string, "{}", built_info::PKG_REPOSITORY).expect("String too long");
+                draw_message_screen_no_reformat(display, &message_string)
+            }
+            _ => draw_message_screen(display, "Unknown page index"),
         }
     }
 }
@@ -43,15 +97,15 @@ impl UiInputHandler for AboutScreen {
         ui_action_publisher: &UiActionChannelPublisher<'_>,
     ) {
         match input {
-            UiInput::EncoderClockwise => self.page_index += 1,
-            UiInput::EncoderCounterClockwise => self.page_index -= 1,
+            UiInput::EncoderClockwise => self.start_entry_index += 1,
+            UiInput::EncoderCounterClockwise => self.start_entry_index -= 1,
             UiInput::ButtonPress => ui_action_publisher.publish_immediate(
                 UiActionsMessage::StateChangeRequest(ApplicationState::Settings),
             ),
             _ => {}
         }
-        if self.page_index >= self.max_pages {
-            self.page_index = 0;
+        if self.start_entry_index >= self.max_entries {
+            self.start_entry_index = 0;
         }
     }
 }
@@ -66,27 +120,22 @@ impl UiDrawer for AboutScreen {
     where
         D: DrawTarget<Color = BinaryColor>,
     {
-        match self.page_index {
-            0 => {
-                let dirty_indicator = if built_info::GIT_DIRTY.unwrap_or(true) {
-                    "+"
-                } else {
-                    ""
-                };
-                let mut message_string = String::<100>::new();
-                write!(
-                    message_string,
-                    "{} v{}\n{}{}\nLicense: {}\n",
-                    built_info::PKG_NAME,
-                    built_info::PKG_VERSION,
-                    built_info::GIT_COMMIT_HASH_SHORT.unwrap_or("unknown"),
-                    dirty_indicator,
-                    built_info::PKG_LICENSE,
-                )
-                .expect("String too long");
-                draw_message_screen_no_reformat(display, &message_string)
-            }
-            _ => draw_message_screen(display, "Unknown page index"),
+        let lines_available = display.bounding_box().size.height / DEFAULT_TEXT_STYLE.line_height();
+        let end_line = min(
+            self.start_entry_index + lines_available as usize,
+            self.max_entries,
+        );
+
+        let display_size = display.bounding_box();
+        let mut line_draw_area =
+            display_size.resized_height(DEFAULT_TEXT_STYLE.line_height(), AnchorY::Top);
+
+        for (line_count, line_index) in (self.start_entry_index..end_line).enumerate() {
+            let mut line_display = display.cropped(&line_draw_area);
+            self.line_draw(&mut line_display, line_index)?;
+            line_draw_area =
+                line_draw_area.translate(Point::new(0, DEFAULT_TEXT_STYLE.line_height() as i32));
         }
+        Ok(())
     }
 }
