05a685cd6bf29704adbef0846e1ec6f4dddab799
Paul Hampson <528002+paulhampson@users.noreply.github.com>
2026-02-22 16:12:10 +0000

    fix: add scrolling for long version and package information strings. refactor: about screen scrolling streams now their own struct.

--- smartcoaster-application/src/hmi/screens/settings_screens/about.rs
+++ smartcoaster-application/src/hmi/screens/settings_screens/about.rs
@@ -32,12 +32,49 @@ use heapless::String;
 const REPOSITORY_PREFIX: &str = "Repository: ";
 const REPOSITORY_SUFFIX: &str = "   ";
 
+#[derive(Debug)]
+struct ScrollingString {
+    pub scroll_pos: u32,
+    pub string: String<80>,
+}
+
+impl ScrollingString {
+    pub fn new(string: String<80>) -> Self {
+        Self { scroll_pos: 0, string }
+    }
+
+    pub fn increment_scroll_pos(&mut self, increment_by: u32) {
+        self.scroll_pos += increment_by;
+        if self.scroll_pos >= self.string.len() as u32 {
+            self.scroll_pos = 0;
+        }
+    }
+
+    /// Calculate the start and end indices of the substring to display based on the screen width, font width, scroll position and string length.
+    /// Will not give a substring that is longer than the string length.
+    /// If no scrolling is required, it will return the indices for the full string.
+    fn get_scroll_text_substring(&self, screen_width: u32, font_width: u32) -> &str {
+        let (start_pos, end_pos) =
+            if (screen_width / font_width) < self.string.len() as u32 {
+                (
+                    self.scroll_pos as usize,
+                    min((self.scroll_pos + (screen_width / font_width)) as usize, self.string.len())
+                )
+            } else {
+                // small enough to fit on screen
+                (0, self.string.len())
+            };
+        &self.string[start_pos..end_pos]
+    }
+}
+
 pub struct AboutScreen {
     start_entry_index: usize,
     max_entries: usize,
 
-    repository_url_scroll_pos: usize,
-    repository_url_string: String<80>,
+    package_information: ScrollingString,
+    repository_url: ScrollingString,
+    version_tag: ScrollingString,
 }
 
 impl AboutScreen {
@@ -50,14 +87,36 @@ impl AboutScreen {
             built_info::PKG_REPOSITORY,
             REPOSITORY_SUFFIX
         )
-        .unwrap();
+            .unwrap();
+
+        let mut package_information_string = String::<80>::new();
+        write!(
+            package_information_string,
+            "{} v{}",
+            built_info::PKG_NAME,
+            built_info::PKG_VERSION,
+        )
+            .expect("Package string too long");
+
+        let mut tag_string = String::<80>::new();
+        if built_info::GIT_VERSION.is_some() {
+            if built_info::GIT_VERSION.unwrap()
+                != built_info::GIT_COMMIT_HASH_SHORT.unwrap()
+            {
+                write!(tag_string, "Tag: {}", built_info::GIT_VERSION.unwrap())
+                    .expect("tag string too long");
+            } else {
+                write!(tag_string, "No tag").expect("tag string too long");
+            }
+        }
 
         let s = Self {
             start_entry_index: 0,
             max_entries: 5,
 
-            repository_url_scroll_pos: 0,
-            repository_url_string,
+            package_information: ScrollingString::new(package_information_string),
+            repository_url: ScrollingString::new(repository_url_string),
+            version_tag: ScrollingString::new(tag_string),
         };
         s
     }
@@ -67,36 +126,25 @@ impl AboutScreen {
     /// to do this here because the draw function is (rightly) not allowed to modify data.
     pub fn update_pre_draw_actions<D>(&mut self, _display: &D)
     where
-        D: DrawTarget<Color = BinaryColor>,
+        D: DrawTarget<Color=BinaryColor>,
     {
-        self.repository_url_scroll_pos += 1;
-        if self.repository_url_scroll_pos >= self.repository_url_string.len() {
-            self.repository_url_scroll_pos = 0;
-        }
+        self.package_information.increment_scroll_pos(1);
+        self.repository_url.increment_scroll_pos(1);
+        self.version_tag.increment_scroll_pos(1);
     }
 
-    fn calc_scroll_text_substring(screen_width: u32, scroll_pos: usize) -> (usize, usize) {
-        (
-            scroll_pos,
-            scroll_pos + (screen_width as usize / DEFAULT_FONT_WIDTH),
-        )
-    }
 
     fn line_draw<D>(&self, display: &mut D, index: usize) -> Result<(), D::Error>
     where
-        D: DrawTarget<Color = BinaryColor>,
+        D: DrawTarget<Color=BinaryColor>,
     {
         let mut message_string = String::<100>::new();
         match index {
             0 => {
-                write!(
-                    message_string,
-                    "{} v{}",
-                    built_info::PKG_NAME,
-                    built_info::PKG_VERSION,
-                )
-                .expect("String too long");
-                draw_message_screen_no_reformat(display, &message_string, Alignment::Center)
+                let sub_string_to_display = self.package_information.get_scroll_text_substring(display.bounding_box().size.width,
+                                                                                               DEFAULT_FONT_WIDTH as u32, );
+                write!(message_string, "{}", sub_string_to_display).expect("String too long");
+                draw_message_screen_no_reformat(display, &message_string, Alignment::Left)
             }
             1 => {
                 let dirty_indicator = if built_info::GIT_DIRTY.unwrap_or(true) {
@@ -106,39 +154,27 @@ impl AboutScreen {
                 };
                 write!(
                     message_string,
-                    "{}{}",
+                    "Commit: {}{}",
                     built_info::GIT_COMMIT_HASH_SHORT.unwrap_or("unknown"),
                     dirty_indicator
                 )
-                .expect("String too long");
-                draw_message_screen_no_reformat(display, &message_string, Alignment::Center)
+                    .expect("String too long");
+                draw_message_screen_no_reformat(display, &message_string, Alignment::Left)
             }
             2 => {
-                if built_info::GIT_VERSION.is_some() {
-                    if built_info::GIT_VERSION.unwrap()
-                        != built_info::GIT_COMMIT_HASH_SHORT.unwrap()
-                    {
-                        write!(message_string, "Tag: {}", built_info::GIT_VERSION.unwrap())
-                            .expect("tag string too long");
-                    } else {
-                        write!(message_string, "No tag").expect("tag string too long");
-                    }
-                }
-                draw_message_screen_no_reformat(display, &message_string, Alignment::Center)
+                let sub_string_to_display = self.version_tag.get_scroll_text_substring(display.bounding_box().size.width,
+                                                                                       DEFAULT_FONT_WIDTH as u32);
+                write!(message_string, "{}", sub_string_to_display).expect("String too long");
+                draw_message_screen_no_reformat(display, &message_string, Alignment::Left)
             }
             3 => {
-                write!(message_string, "License: {}", built_info::PKG_LICENSE,)
+                write!(message_string, "License: {}", built_info::PKG_LICENSE, )
                     .expect("String too long");
                 draw_message_screen_no_reformat(display, &message_string, Alignment::Left)
             }
             4 => {
-                let (start_str_idx, end_str_idx) = Self::calc_scroll_text_substring(
-                    display.bounding_box().size.width,
-                    self.repository_url_scroll_pos,
-                );
-                let capped_end_str_idx = min(end_str_idx, self.repository_url_string.len());
-                let sub_string_to_display =
-                    &self.repository_url_string[start_str_idx..capped_end_str_idx];
+                let sub_string_to_display = self.repository_url.get_scroll_text_substring(display.bounding_box().size.width,
+                                                                                          DEFAULT_FONT_WIDTH as u32);
                 write!(message_string, "{}", sub_string_to_display).expect("String too long");
                 draw_message_screen_no_reformat(display, &message_string, Alignment::Left)
             }
@@ -174,7 +210,7 @@ mod built_info {
 impl UiDrawer for AboutScreen {
     fn draw<D>(&self, display: &mut D) -> Result<(), D::Error>
     where
-        D: DrawTarget<Color = BinaryColor>,
+        D: DrawTarget<Color=BinaryColor>,
     {
         let lines_available = display.bounding_box().size.height / DEFAULT_TEXT_STYLE.line_height();
         let end_line = min(
